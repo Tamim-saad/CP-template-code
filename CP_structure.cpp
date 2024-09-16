446443eb99d001cd89da4f262261515d16e7b20e
Tamim Hasan Saad <136108639+Tamim-saad@users.noreply.github.com>
2024-09-16 13:28:54 +0600

    Update CP_structure.cpp

--- CP_structure.cpp
+++ CP_structure.cpp
@@ -446,18 +446,32 @@ ll gcd(ll arr[], ll n) {
   return ans;
 }
 
-const int mod = 1e9 + 7;
-ll modular_exponent(ll base, ll pow) {
+//---------compute factorial with modular exponent--------------------------------------------------------
+ll modular_exponent(ll base, ll pow, ll mod) {
   if (pow == 0)
     return 1;
 
-  ll x = modular_exponent(base, pow / 2);
+  ll x = modular_exponent(base, pow / 2, mod);
 
   if (pow % 2 == 0)
     return (x * x) % mod;
   else
     return ((x * x) % mod * base) % mod;
 }
+
+void compute_factorial_with_modular_inverse(ll mod, vector<ll> &factorials,
+                       vector<ll> &factorials_mod_inv) {
+  factorials[0] = 1;
+  int n = factorials.size() - 1;
+  for (int i = 1; i <= n; i++) {
+    factorials[i] = factorials[i - 1] * i % mod;
+  }
+  factorials_mod_inv[n] = modular_exponent(factorials[n], mod - 2, mod);
+  for (int i = n - 1; i >= 0; i--) {
+    factorials_mod_inv[i] = factorials_mod_inv[i + 1] * (i + 1) % mod;
+  }
+}
+//---------------------------------------------------------------------------------
 
 // string binary = bitset<8>(n).to_string();
 // unsigned long decimal =bitset<8>(binary).to_ulong();
